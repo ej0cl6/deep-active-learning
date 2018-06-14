6f383ab17b3df0f747ef9e570b54d2336bd98b42
ej0cl6 <ej0cl6@hotmail.com>
2018-06-14 13:20:05 +0800

    accelerate kcenter_greedy

--- query_strategies/kcenter_greedy.py
+++ query_strategies/kcenter_greedy.py
@@ -7,19 +7,30 @@ class KCenterGreedy(Strategy):
 		super(KCenterGreedy, self).__init__(X, Y, idxs_lb, args)
 
 	def query(self, n):
-		idxs_lb_copy = self.idxs_lb.copy()
+		lb_flag = self.idxs_lb.copy()
 		embedding = self.get_embedding(self.X, self.Y)
 		embedding = embedding.numpy()
+
+		from datetime import datetime
+
+		print('calculate distance matrix')
+		t_start = datetime.now()
+		dist_mat = np.matmul(embedding, embedding.transpose())
+		sq = np.array(dist_mat.diagonal()).reshape(len(self.X), 1)
+		dist_mat *= -2
+		dist_mat += sq
+		dist_mat += sq.transpose()
+		dist_mat = np.sqrt(dist_mat)
+		print(datetime.now() - t_start)
+
+		mat = dist_mat[~lb_flag, :][:, lb_flag]
+
 		for i in range(n):
-			idxs_lb = np.arange(self.n_pool)[idxs_lb_copy]
-			idxs_ub = np.arange(self.n_pool)[~idxs_lb_copy]
-			embedding_lb = embedding[idxs_lb]
-			embedding_ub = embedding[idxs_ub]
+			mat_min = mat.min(axis=1)
+			q_idx_ = mat_min.argmax()
+			q_idx = np.arange(self.n_pool)[~lb_flag][q_idx_]
+			lb_flag[q_idx] = True
+			mat = np.delete(mat, q_idx_, 0)
+			mat = np.append(mat, dist_mat[~lb_flag, q_idx][:, None], axis=1)
 
-			NN = NearestNeighbors(n_neighbors=1)
-			NN.fit(embedding_lb)
-			nn_dis, nn_idxs = NN.kneighbors(embedding_ub)
-			q_idx = idxs_ub[nn_dis.argmax()]
-			idxs_lb_copy[q_idx] = True
-
-		return np.arange(self.n_pool)[(self.idxs_lb ^ idxs_lb_copy)]
+		return np.arange(self.n_pool)[(self.idxs_lb ^ lb_flag)]
