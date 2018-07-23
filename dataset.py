e73fd0df133612feaf32f99fff7991dd730d68ba
ej0cl6 <ej0cl6@gmail.com>
2018-07-23 16:23:46 +0800

    dataset, model

---
+++ dataset.py
@@ -0,0 +1,109 @@
+import numpy as np
+import torch
+from torchvision import datasets
+from torch.utils.data import Dataset
+from PIL import Image
+
+def get_dataset(name):
+    if name == 'MNIST':
+        return get_MNIST()
+    elif name == 'FashionMNIST':
+        return get_FashionMNIST()
+    elif name == 'SVHN':
+        return get_SVHN()
+    elif name == 'CIFAR10':
+        return get_CIFAR10()
+
+def get_MNIST():
+    raw_tr = datasets.MNIST('./MNIST', train=True, download=True)
+    raw_te = datasets.MNIST('./MNIST', train=False, download=True)
+    X_tr = raw_tr.train_data
+    Y_tr = raw_tr.train_labels
+    X_te = raw_te.test_data
+    Y_te = raw_te.test_labels
+    return X_tr, Y_tr, X_te, Y_te
+
+def get_FashionMNIST():
+    raw_tr = datasets.FashionMNIST('./FashionMNIST', train=True, download=True)
+    raw_te = datasets.FashionMNIST('./FashionMNIST', train=False, download=True)
+    X_tr = raw_tr.train_data
+    Y_tr = raw_tr.train_labels
+    X_te = raw_te.test_data
+    Y_te = raw_te.test_labels
+    return X_tr, Y_tr, X_te, Y_te
+
+def get_SVHN():
+    data_tr = datasets.SVHN('./SVHN', split='train', download=True)
+    data_te = datasets.SVHN('./SVHN', split='test', download=True)
+    X_tr = data_tr.data
+    Y_tr = torch.from_numpy(data_tr.labels)
+    X_te = data_te.data
+    Y_te = torch.from_numpy(data_te.labels)
+    return X_tr, Y_tr, X_te, Y_te
+
+def get_CIFAR10():
+    data_tr = datasets.CIFAR10('./CIFAR10', train=True, download=True)
+    data_te = datasets.CIFAR10('./CIFAR10', train=False, download=True)
+    X_tr = data_tr.train_data
+    Y_tr = torch.from_numpy(np.array(data_tr.train_labels))
+    X_te = data_te.test_data
+    Y_te = torch.from_numpy(np.array(data_te.test_labels))
+    return X_tr, Y_tr, X_te, Y_te
+
+def get_handler(name):
+    if name == 'MNIST':
+        return DataHandler1
+    elif name == 'FashionMNIST':
+        return DataHandler1
+    elif name == 'SVHN':
+        return DataHandler2
+    elif name == 'CIFAR10':
+        return DataHandler3
+
+class DataHandler1(Dataset):
+    def __init__(self, X, Y, transform=None):
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.X[index], self.Y[index]
+        if self.transform is not None:
+            x = Image.fromarray(x.numpy(), mode='L')
+            x = self.transform(x)
+        return x, y, index
+
+    def __len__(self):
+        return len(self.X)
+
+class DataHandler2(Dataset):
+    def __init__(self, X, Y, transform=None):
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.X[index], self.Y[index]
+        if self.transform is not None:
+            x = Image.fromarray(np.transpose(x, (1, 2, 0)))
+            x = self.transform(x)
+        return x, y, index
+
+    def __len__(self):
+        return len(self.X)
+
+class DataHandler3(Dataset):
+    def __init__(self, X, Y, transform=None):
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.X[index], self.Y[index]
+        if self.transform is not None:
+            x = Image.fromarray(x)
+            x = self.transform(x)
+        return x, y, index
+
+    def __len__(self):
+        return len(self.X)
---
+++ model.py
@@ -0,0 +1,80 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+def get_net(name):
+    if name == 'MNIST':
+        return Net1
+    elif name == 'FashionMNIST':
+        return Net1
+    elif name == 'SVHN':
+        return Net2
+    elif name == 'CIFAR10':
+        return Net3
+
+class Net1(nn.Module):
+    def __init__(self):
+        super(Net1, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        e1 = F.relu(self.fc1(x))
+        x = F.dropout(e1, training=self.training)
+        x = self.fc2(x)
+        return x, e1
+
+    def get_embedding_dim(self):
+        return 50
+
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3)
+        self.conv3_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(1152, 400)
+        self.fc2 = nn.Linear(400, 50)
+        self.fc3 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
+        x = x.view(-1, 1152)
+        x = F.relu(self.fc1(x))
+        e1 = F.relu(self.fc2(x))
+        x = F.dropout(e1, training=self.training)
+        x = self.fc3(x)
+        return x, e1
+
+    def get_embedding_dim(self):
+        return 50
+
+class Net3(nn.Module):
+    def __init__(self):
+        super(Net3, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=5)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=5)
+        self.fc1 = nn.Linear(1024, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 1024)
+        e1 = F.relu(self.fc1(x))
+        x = F.dropout(e1, training=self.training)
+        x = self.fc2(x)
+        return x, e1
+
+    def get_embedding_dim(self):
+        return 50
